595a12f34e75397d679648a2fe3ed23fc07ff86c
devGiyosiddin <devgiyosiddin@gmail.com>
2024-03-09 20:32:09 +0500

    Some changes for project

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+const elMenuButton = document.querySelector('.js-menu-button');
+const elSiteActions = document.querySelector('.site-header-actions');
+const elSiteHeader = document.querySelector('#site-header');
+if(elMenuButton) {
+    elMenuButton.addEventListener('click', function() {
+    elSiteActions.classList.toggle('menu-list--open');
+    elSiteHeader.classList.toggle('bgcolor');
+    });
+};
+const elsSitenavLink = document.querySelectorAll('.sitenav-link');
+elsSitenavLink.forEach(function (elLink) {
+    elLink.addEventListener('click', function() {
+    elSiteActions.classList.remove('menu-list--open');
+    elMenuButton.classList.remove('menu--open');
+    elSiteHeader.classList.remove('bgcolor');
+    });
+});
+const elMainLink = document.querySelector('.main-link');
+if(elMainLink) {
+    elMainLink.addEventListener('click', function() {
+    elSiteActions.classList.remove('menu-list--open');
+    elMenuButton.classList.remove('menu--open');
+    });
+};
