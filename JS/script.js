fd2e391d4b34d83a977fbab38f6f73816d700081
devGiyosiddin <devgiyosiddin@gmail.com>
2024-03-20 02:12:36 +0500

    some script change

--- JS/script.js
+++ JS/script.js
@@ -22,13 +22,3 @@ if(elMainLink) {
     elMenuButton.classList.remove('menu--open');
     });
 };
-
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
