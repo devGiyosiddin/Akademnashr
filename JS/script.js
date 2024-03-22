5f5deb8c86eab944f5a92de9e6a85e1c8d59b6b8
devGiyosiddin <devgiyosiddin@gmail.com>
2024-03-22 09:50:58 +0500

    Add function to detect user's theme

--- JS/script.js
+++ JS/script.js
@@ -43,8 +43,32 @@ const container = document.getElementById('container'),
             })
         })
 
-// Theme switcher
+
+// THEME SWITCHER
 const themeSwitcher = document.querySelector('#theme-switcher');
 themeSwitcher.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
+});
+
+// Проверяем, поддерживает ли браузер Media Query prefers-color-scheme
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // Если пользователь предпочитает темную тему, добавляем класс dark-theme к <body>
+    document.body.classList.add('dark-theme');
+} else {
+    // В противном случае, если пользователь предпочитает светлую тему, удаляем класс dark-theme
+    document.body.classList.remove('dark-theme');
+}
+
+// Обработчик события изменения темы у пользователя
+window.matchMedia('(prefers-color-scheme: dark)').addListener((e) => {
+    const darkModeOn = e.matches;
+    // Если darkModeOn === true, пользователь предпочитает темную тему
+    // Если darkModeOn === false, пользователь предпочитает светлую тему
+    if (darkModeOn) {
+        // Добавляем класс dark-theme к <body>
+        document.body.classList.add('dark-theme');
+    } else {
+        // Удаляем класс dark-theme из <body>
+        document.body.classList.remove('dark-theme');
+    }
 });
